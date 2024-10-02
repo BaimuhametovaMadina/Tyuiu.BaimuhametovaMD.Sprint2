f6a28dae90ecd82580ff6087a716683f59b7b472
BaimukhametovaMD <Tyuiu.BaimuhametovaMD@gmail.com>
2024-10-02 13:18:15 +0500

    task3

---
+++ Tyuiu.BaimuhametovaMD.Sprint2.Task3.V5.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.BaimuhametovaMD.Sprint2.Task3.V5.Lib
+{
+    public class DataService: ISprint2Task3V5
+    {
+        public double Calculate(double x)
+        {
+            if (x > 1) return Math.Round((x - Math.Pow((x + 1) / (x - 1), x)), 3);
+            else if (x == 0) return Math.Round(((x * x - Math.Cos(x * x)) / (x * x - Math.Sin(x * x) + 12)), 3);
+            else if (x < 0 && x > -9) return Math.Round(Math.Pow(6 + (4 / (x * x)), x), 3);
+            else if (x < -9) return Math.Round((x * x * x + 10 * x - ((x * x) / (x * x * x * x))), 3);
+            else return 0;
+        }
+    }
+}
---
+++ Tyuiu.BaimuhametovaMD.Sprint2.Task3.V5/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.BaimuhametovaMD.Sprint2.Task3.V5.Lib;
+
+namespace Tyuiu.BaimuhametovaMD.Sprint2.Task3.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Баймухаметова М.Д. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Баймухаметова Мадина | ИБКСб-24-1                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else,                            *");
+            Console.WriteLine("* где пользователь вводит значение переменной X с клавиатуры.             *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (x > 0 && x <= 1 || x ==9) Console.WriteLine("Функции для такого значения X не существует");
+            else
+            {
+                Console.Write("Значение функции Y при данном значении X: ");
+                double res = ds.Calculate(x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
