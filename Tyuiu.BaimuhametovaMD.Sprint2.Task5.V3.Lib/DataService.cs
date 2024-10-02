e111eefa58d801addb4cf6165b7a06f4bbcadc9f
BaimukhametovaMD <Tyuiu.BaimuhametovaMD@gmail.com>
2024-10-02 13:44:54 +0500

    task5

---
+++ Tyuiu.BaimuhametovaMD.Sprint2.Task5.V3.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.BaimuhametovaMD.Sprint2.Task5.V3.Lib
+{
+    public class DataService: ISprint2Task5V3
+    {
+        public string FindDayName(int value)
+        {
+            switch (value)
+            {
+                case 1:
+                    return "Понедельник";
+                    break;
+                case 2:
+                    return "Вторник";
+                    break;
+                case 3:
+                    return "Среда";
+                    break;
+                case 4:
+                    return "Четверг";
+                    break;
+                case 5:
+                    return "Пятница";
+                    break;
+                case 6:
+                    return "Суббота";
+                    break;
+                case 7:
+                    return "Воскресенье";
+                    break;
+                default: return "Дня с таким номером не существует";
+            }
+        }
+    }
+}
---
+++ Tyuiu.BaimuhametovaMD.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.BaimuhametovaMD.Sprint2.Task5.V3.Lib;
+
+namespace Tyuiu.BaimuhametovaMD.Sprint2.Task5.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Баймухаметова М.Д. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Баймухаметова Мадина | ИБКСб-24-1                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch                  *");
+            Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Составить программу, которая в зависимости от порядкового номера        *");
+            Console.WriteLine("* дня недели (1, 2, …, 7) выводит на экран его название                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите номер дня недели: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write(ds.FindDayName(x));
+            Console.ReadLine();
+        }
+    }
+}
